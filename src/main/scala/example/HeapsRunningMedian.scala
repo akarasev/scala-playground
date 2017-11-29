8f8584e3ecb124b7946326c8f88ffe105b9ed335
Alex Karasev <alex.v.karasev@gmail.com>
2017-11-29 17:17:31 +0700

    Added a sample of working with heaps

---
+++ src/main/scala/example/HeapsRunningMedian.scala
@@ -0,0 +1,52 @@
+package example
+
+import scala.collection.mutable
+
+/**
+  * <h3>Heaps: Find the Running Median</h3>
+  * <p>
+  *   The median of a dataset of integers is the midpoint value of the dataset
+  *   for which an equal number of integers are less than and greater than the value.
+  *   To find the median, it's needed first sort the dataset of integers in
+  *   non-decreasing order, then:
+  * </p>
+  * <ol>
+  *   <li>
+  *     If the dataset contains an odd number of elements, the median is
+  *     the middle element of the sorted sample.
+  *     In the sorted dataset {1, 2, 3}, 2 is the median.
+  *   </li>
+  *   <li>
+  *     If the dataset contains an even number of elements,
+  *     the median is the average of the two middle elements of the sorted sample.
+  *     In the sorted dataset {1, 2, 3, 4}, (2 + 3)/2 = 2.5 is the median.
+  *   </li>
+  * </ol>
+  * <p>
+  *   Given list of integers, perform the following task:
+  * </p>
+  * <ol>
+  *   <li>Add the i<sup>th</sup> to a running list of integers.</li>
+  *   <li>Find the median of the updated list</li>
+  *   <li>
+  *     Return all medians as a list.
+  *     The medians must be double-precision numbers scaled to decimal place.
+  *   </li>
+  * </ol>
+  */
+object HeapsRunningMedian {
+
+  private val minHeap = mutable.PriorityQueue.empty[Int]
+
+  private val maxHeap = mutable.PriorityQueue.empty[Int].reverse
+
+  /**
+    * Simulate adding integers one by one and return all medians of the list.
+    *
+    * @param items list of integers.
+    * @return all medians.
+    */
+  def getMedians(items: List[Int]): List[Double] = {
+    ???
+  }
+}
