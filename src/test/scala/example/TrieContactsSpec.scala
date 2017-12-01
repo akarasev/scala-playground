5a1e55d398fec363a95401d15c9d3ae3dacda1ec
Alex Karasev <alex.v.karasev@gmail.com>
2017-12-01 16:24:55 +0700

    Added sample of working with Tries (enhanced test suite).

--- src/test/scala/example/TrieContactsSpec.scala
+++ src/test/scala/example/TrieContactsSpec.scala
@@ -1,13 +1,19 @@
 package example
 
-import org.scalatest.{FlatSpec, Matchers}
+import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
 
-class TrieContactsSpec extends FlatSpec with Matchers {
+class TrieContactsSpec extends FlatSpec with Matchers with BeforeAndAfter {
 
-  "After adding 'some' and 'something' the output for 'some' and 'somebody'" should "be 2 and 0" in {
+  before {
     TrieContacts.add("some")
     TrieContacts.add("something")
+  }
+
+  "The word 'some'" should "have two matches" in {
     TrieContacts.find("some") shouldEqual 2
+  }
+
+  "The word 'somebody'" should "have no matches" in {
     TrieContacts.find("somebody") shouldEqual 0
   }
 }
